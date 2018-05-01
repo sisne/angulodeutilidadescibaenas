cead200b6541430b106b1c2f220bdf618ce69be8
Marco Cabral <marco@sisne.do>
2018-05-01 18:56:32 -0400

    Very small l10n_sisne

--- l10n_sisne/__manifest__.py
+++ l10n_sisne/__manifest__.py
@@ -5,36 +5,15 @@
     'name': 'Dominican Republic - Accounting',
     'version': '1.0',
     'category': 'Localization',
-    'description':
-
-        """
-
-This is a base module to manage the accounting for Dominican Republic.
-======================================================================
-
-* The main taxes used in Domincan Republic
-* Fiscal position for local
-
-        """,
-    
+    'description': '',
     'author': 'SISNE, SRL.',
     'website': 'http://sisne.do',
     'depends': ['account', 'base_iban'],
     'data': [
-
-        """
-
         'data/account_chart_template.xml',
-        'data/account_tax_template.xml',
-        'data/account_fiscal_position_template.xml',
-        'data/account_fiscal_position_tax_template.xml',
         'data/l10n_do_base_data.xml',
         'data/account_chart_template.yml',
-
-        """
-
     ],
-    
     'test': [],
     'demo': [],
     'installable': True,
